c0e89c66a310c8b069a8dbf20cb1a4bdc89c6831
Akash NP <akashnp1998@gmail.com>
2022-10-16 19:25:51 +0530

    Update Searchbar Styling for Dark mode

--- components/HomeMain.js
+++ components/HomeMain.js
@@ -8,10 +8,10 @@ function HeroTitle() {
 
 function SearchInputGroup() {
     return (
-        <form className='py-10'>
+        <form className='py-20'>
             <div className="flex justify-center">
                 <div className="relative w-1/2">
-                    <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Books..." required=""/>
+                    <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-slate-100 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-700 dark:text-gray dark:focus:border-slate-100" placeholder="Enter Book Name & [TAB + ENTER] to search" required=""/>
                 </div>
             </div>
         </form>
